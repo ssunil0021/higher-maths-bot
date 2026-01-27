e84a289a6d7fd7cbc03dd42628ba0f456e71ce90
Anil <ssunil0021@gmail.com>
2026-01-27 12:44:24 +0530

    by ANIL

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+import os
+from flask import Flask, request
 import telebot
+
 from config import BOT_TOKEN, PARSE_MODE
 from handlers import register_handlers
 
 bot = telebot.TeleBot(BOT_TOKEN, parse_mode=PARSE_MODE)
-
 register_handlers(bot)
 
-print("🤖 Bot running...")
+app = Flask(__name__)
 
-bot.infinity_polling(
-    timeout=30,
-    long_polling_timeout=30,
-    skip_pending=True
-)
+WEBHOOK_PATH = f"/{BOT_TOKEN}"
+WEBHOOK_URL = os.getenv("WEBHOOK_URL") + WEBHOOK_PATH
+
+# set webhook
+bot.remove_webhook()
+bot.set_webhook(url=WEBHOOK_URL)
+
+@app.route(WEBHOOK_PATH, methods=["POST"])
+def webhook():
+    json_str = request.get_data().decode("UTF-8")
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "OK", 200
+
+@app.route("/")
+def home():
+    return "Bot is running", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
