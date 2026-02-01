11eac19fdceec65ea488201c7390cbc8319f134d
Anil <ssunil0021@gmail.com>
2026-02-01 17:31:28 +0530

    a

---
+++ books/books.py
@@ -0,0 +1,16 @@
+BOOKS = [
+    {
+        "id": "rudin_real",
+        "name": "Principles of Mathematical Analysis",
+        "author": "Walter Rudin",
+        "keywords": ["analysis", "real analysis", "rudin", "baby rudin"],
+        "link": "https://drive.google.com/..."
+    },
+    {
+        "id": "axler_la",
+        "name": "Linear Algebra Done Right",
+        "author": "Sheldon Axler",
+        "keywords": ["linear algebra", "axler", "la"],
+        "link": "https://drive.google.com/..."
+    }
+]
